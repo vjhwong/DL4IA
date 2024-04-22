76c4025f3426f108568983b8350873a35a8b5729
Victor Wong <victor.ju.hin.wong@gmail.com>
2024-04-22 12:56:21 +0200

    Refactor load_warwick function in utils.py to improve image loading efficiency

--- Assignment 3/utils.py
+++ Assignment 3/utils.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import time
 import numpy as np
+import imageio.v2 as imageio
+import glob
 
 
 def measure_runtime(func):
@@ -217,3 +219,36 @@ def find_misclassified_indices(
                 (predicted != torch.argmax(labels, dim=1)).nonzero().squeeze().tolist()
             )
     return torch.tensor(misclassified_indices)
+
+
+def load_warwick():
+
+    train_images = []
+    train_labels = []
+    test_images = []
+    test_labels = []
+
+    for image_path in sorted(glob.glob("WARWICK/WARWICK" + "/Train/image_*.png")):
+        image = imageio.imread(image_path)
+        train_images.append(image)
+    for label_path in sorted(glob.glob("WARWICK/WARWICK" + "/Train/label_*.png")):
+        label = imageio.imread(label_path)
+        train_labels.append(label)
+
+    for image_path in sorted(glob.glob("WARWICK/WARWICK" + "/Test/image_*.png")):
+        image = imageio.imread(image_path)
+        test_images.append(image)
+    for label_path in sorted(glob.glob("WARWICK/WARWICK" + "/Test/label_*.png")):
+        label = imageio.imread(label_path)
+        test_labels.append(label)
+
+    X_train = torch.tensor(
+        np.array(train_images), dtype=torch.float, requires_grad=True
+    ).permute(0, 3, 1, 2)
+    Y_train = torch.tensor(np.array(train_labels), dtype=torch.long) / 255
+    X_test = torch.tensor(
+        np.array(test_images), dtype=torch.float, requires_grad=True
+    ).permute(0, 3, 1, 2)
+    Y_test = torch.tensor(np.array(test_labels), dtype=torch.long) / 255
+
+    return X_train, Y_train, X_test, Y_test
