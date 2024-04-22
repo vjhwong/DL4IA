ffb8d35c137839d25751f8d6dbd49ef59c365c5e
Victor Wong <victor.ju.hin.wong@gmail.com>
2024-04-22 12:45:19 +0200

    push

--- Assignment 3/utils.py
+++ Assignment 3/utils.py
@@ -2,6 +2,20 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import time
+import numpy as np
+
+
+def measure_runtime(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        runtime = end_time - start_time
+        print(f"Runtime of {func.__name__}: {runtime:.4f} seconds")
+        return result
+
+    return wrapper
 
 
 def plot_learning_curves(
@@ -30,16 +44,16 @@ def plot_learning_curves(
     axs[0].plot(
         range(1, num_epochs + 1),
         train_losses,
-        label=f"Final train loss: {train_losses[-1]:.4f}",
+        label=f"Final train cost: {train_losses[-1]:.4f}",
     )
     axs[0].plot(
         range(1, num_epochs + 1),
         test_losses,
-        label=f"Final test loss: {test_losses[-1]:.4f}",
+        label=f"Final test cost: {test_losses[-1]:.4f}",
     )
     axs[0].set_xlabel("Epoch", fontsize=md)
-    axs[0].set_ylabel("Loss", fontsize=md)
-    axs[0].set_title("Training and Test Loss", fontsize=md)
+    axs[0].set_ylabel("Cost", fontsize=md)
+    axs[0].set_title("Cost", fontsize=md)
     axs[0].legend(fontsize=sm)
     axs[0].tick_params(labelsize=sm)
     axs[0].grid(True, which="both", linestyle="--", linewidth=0.5)
@@ -55,8 +69,8 @@ def plot_learning_curves(
         label=f"Final test accuracy: {test_accuracies[-1]:.4f}%",
     )
     axs[1].set_xlabel("Epoch", fontsize=md)
-    axs[1].set_ylabel("Accuracy", fontsize=md)
-    axs[1].set_title("Training and Test Accuracy", fontsize=md)
+    axs[1].set_ylabel("Accuracy (%)", fontsize=md)
+    axs[1].set_title("Accuracy", fontsize=md)
     axs[1].legend(fontsize=sm)
     axs[1].tick_params(labelsize=sm)
     axs[1].grid(True, which="both", linestyle="--", linewidth=0.5)
@@ -65,6 +79,7 @@ def plot_learning_curves(
     plt.show()
 
 
+@measure_runtime
 def train_neural_network(
     model: nn.Module,
     criterion: nn.Module,
@@ -73,6 +88,7 @@ def train_neural_network(
     test_loader: DataLoader,
     num_epochs: int,
     device: torch.device,
+    model_name: str,
 ) -> None:
     """Train a neural network model and evaluate it on a test set.
 
@@ -85,6 +101,7 @@ def train_neural_network(
         num_epochs (int): Number of epochs
         device (torch.device): Device to run the model on
     """
+    start = time.time()
     train_costs = []
     train_accuracies = []
     test_costs = []
@@ -97,7 +114,7 @@ def train_neural_network(
         total_train = 0
 
         # Training loop
-        for i, (images, labels) in enumerate(train_loader):
+        for images, labels in train_loader:
             # Move tensors to the configured device
             images = images.to(device)
             labels = labels.to(device)
@@ -115,11 +132,6 @@ def train_neural_network(
             _, predicted = torch.max(outputs.data, 1)
             total_train += labels.size(0)
             correct_train += (predicted == torch.argmax(labels, dim=1)).sum().item()
-
-            # if (i + 1) % 100 == 0:x
-            #     print(
-            #         f"Epoch {epoch+1}/{num_epochs}, Step {i+1}/{len(train_loader)}, Loss: {loss.item():.4f}"
-            #     )
 
         train_costs.append(train_loss / len(train_loader))
         train_accuracies.append(100 * (correct_train / total_train))
@@ -144,14 +156,64 @@ def train_neural_network(
                 total_test += labels.size(0)
                 correct_test += (predicted == torch.argmax(labels, dim=1)).sum().item()
 
-        test_costs.append(test_loss / len(test_loader))
-        test_accuracies.append(100 * (correct_test / total_test))
+            test_costs.append(test_loss / len(test_loader))
+            test_accuracies.append(100 * (correct_test / total_test))
 
         print(
-            f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_costs[-1]:.4f}, Train Accuracy: {train_accuracies[-1]:.2f}%, Test Loss: {test_costs[-1]:.4f}, Test Accuracy: {test_accuracies[-1]:.2f}%"
+            f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_costs[-1]:.4f}, Test Loss: {test_costs[-1]:.4f}, Train Accuracy: {train_accuracies[-1]:.2f}%, Test Accuracy: {test_accuracies[-1]:.2f}%"
         )
-
-    title = f"Learning rate: {optimizer.param_groups[0]['lr']}, Batch size: {train_loader.batch_size}, Optimizer: {type(optimizer).__name__}"
+    end_time = time.time()
+    title = f"{model_name}\nLearning rate: {optimizer.param_groups[0]['lr']}, Batch size: {train_loader.batch_size}, Optimizer: {type(optimizer).__name__}, Time: {end_time - start:.2f} seconds"
     plot_learning_curves(
         num_epochs, train_costs, test_costs, train_accuracies, test_accuracies, title
     )
+
+
+def compute_confusion_matrix(predictions: torch.Tensor, true_labels: torch.Tensor):
+    """Compute the confusion matrix for a set of predictions and true labels.
+
+    Args:
+        predictions (torch.Tensor): Predictions
+        true_labels (torch.Tensor): True labels
+
+    Returns:
+        torch.Tensor: Confusion matrix
+    """
+    predictions = predictions.argmax(dim=1)
+    true_labels = true_labels.argmax(dim=1)
+    matrix = torch.zeros(10, 10, dtype=torch.int32).to(predictions.device)
+    for datapoint in range(len(true_labels)):
+        prediction = predictions[datapoint]
+        true_class = true_labels[datapoint]
+        matrix[prediction, true_class] += 1
+    return matrix
+
+
+def find_misclassified_indices(
+    model: nn.Module, test_loader: DataLoader, device: torch.device
+) -> torch.Tensor:
+    """Find the indices of misclassified images in a test set.
+
+    Args:
+        model (nn.Module): Model to evaluate
+        test_loader (DataLoader): Test data loader
+        device (torch.device): Device to run the model on
+
+    Returns:
+        torch.Tensor: Indices of misclassified images
+    """
+    misclassified_indices = []
+    model.eval()
+    with torch.no_grad():
+        for images, labels in test_loader:
+            # Move tensors to the configured device
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            misclassified_indices.extend(
+                (predicted != torch.argmax(labels, dim=1)).nonzero().squeeze().tolist()
+            )
+    return torch.tensor(misclassified_indices)
